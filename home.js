3d7b3b2df238a2123740340cc3d60c706c3c315f
unknown <you@example.com>
2024-09-24 22:54:38 +0600

    added new code

--- home.js
+++ home.js
@@ -7,28 +7,30 @@ document.getElementById('blog-btn').addEventListener('click',function(){
 })
 document.getElementById('donate-now').addEventListener('click',function(){
     
-    const inputValu = getElementByInputValue('input-value');
-    if(isNaN(inputValu)){
+    const inputValue = getElementByInputValue('input-value');
+    if(isNaN(inputValue)){
         alert('Its not a number please type number ');
         return;
     }
-    const textValu = getElementByIdTextValue('donate-blance');
-    const newDonate = inputValu + textValu;
-    if(inputValu > mainBlance){
-        alert('You do not have enough money ')
-    }
-    document.getElementById('donate-blance').innerText = newDonate;
     const mainBlance = getElementByIdTextValue('main-blace');
-    const upadateBlance = mainBlance - inputValu;
+    if(inputValue > mainBlance){
+        alert('You do not have enough money');
+        return;
+    }
+    if(inputValue <= 0){
+        alert('Negative Value is not Allowed');
+        return;
+    }
+
+    const textValue = getElementByIdTextValue('donate-blance');
+    const newDonate = inputValue + textValue;
+    document.getElementById('donate-blance').innerText = newDonate;
+    const upadateBlance = mainBlance - inputValue;
     document.getElementById('main-blace').innerText = upadateBlance;
 
-
-    
     const div = document.createElement('div');
-    div.innerHTML = `<p class="text-2xl font-bold text-black border-2 px-4 py-3 rounded-2xl mb-6"> ${inputValu} Taka is Donated for famine-2024 at Feni, Bangladesh </p>`
-    
-    
-    
+    div.innerHTML = `<p class="text-2xl font-bold text-black border-2 px-4 py-3 rounded-2xl mb-6"> ${inputValue} Taka is Donated for famine-2024 at Feni, Bangladesh </p>`
+ 
     document.getElementById('transaction-container').appendChild(div);
 
 
@@ -36,25 +38,64 @@ document.getElementById('donate-now').addEventListener('click',function(){
 
 
 
-// document.getElementById('donate-now').addEventListener('click', function() {
-//     const inputValue = getElementByInputValue('input-value');
-//     const mainBalance = getElementByIdTextValue('main-balance');
+// secound section code 
+document.getElementById('flood-donate').addEventListener('click', function(){
+    const floodInputValue = getElementByInputValue('flood-input-blance');
+    if(isNaN (floodInputValue)){
+        alert('Its not a number please type number  ');
+        return
+    }
+    const mainBlance = getElementByIdTextValue('main-blace');
+    if(floodInputValue > mainBlance){
+        alert ('You do not have  enough blance ')
+        return;
+    }
+    if( floodInputValue <= 0){
+        alert('Negative mark is not count')
+        return
+    }
 
-//     if (inputValue > mainBalance) {
-//         alert('You do not have enough money');
-//         return;
-//     }
+    
+    const textFloodValue = getElementByIdTextValue('flood-add-blance');
+    const newFloodValue = floodInputValue + textFloodValue;
+    document.getElementById('flood-add-blance').innerText = newFloodValue;
 
-//     if (isNaN(inputValue)) {
-//         alert('It\'s not a number, please type a valid number');
-//         return;
-//     }
+    const floodUpdateBlance = mainBlance - floodInputValue;
+    document.getElementById('main-blace').innerText = floodUpdateBlance;
 
-//     const donateBalance = getElementByIdTextValue('donate-balance');
-//     const newDonateBalance = inputValue + donateBalance;
-//     document.getElementById('donate-balance').innerText = newDonateBalance;
+    const div = document.createElement('div');
+    div.innerHTML = `<p class="text-2xl font-bold text-black border-2 px-4 py-3 rounded-2xl mb-6"> ${floodInputValue} Taka is Donated for Flood Relief in Feni, Bangladesh </p>`
+ 
+    document.getElementById('transaction-container').appendChild(div);
 
-//     const newMainBalance = mainBalance - inputValue;
-//     document.getElementById('main-balance').innerText = newMainBalance;
-// });
 
+})
+
+document.getElementById('quata-donate').addEventListener('click', function(){
+    const quataInputValue = getElementByInputValue('quata-input');
+    if(quataInputValue <=0){
+        alert('Nagetive mark its not count')
+        return;
+
+    }
+    if(isNaN(quataInputValue)){
+        alert('please type a number ')
+        return;
+    }
+    const textQuateValue = getElementByIdTextValue('quata-blance');
+    const quataBlance = quataInputValue + textQuateValue;
+    document.getElementById('quata-blance').innerText = quataBlance;
+
+    const mainBlance = getElementByIdTextValue('main-blace');
+    if(quataInputValue > mainBlance){
+        alert('You do not have enough money ')
+    }
+    const updateQuataBlance = mainBlance - quataInputValue;
+    document.getElementById('main-blace').innerText = updateQuataBlance;
+
+    const div = document.createElement('div');
+    div.innerHTML = `<p class="text-2xl font-bold text-black border-2 px-4 py-3 rounded-2xl mb-6"> ${quataInputValue} Taka is Donated for Flood Relief in Quata, Bangladesh </p>`
+ 
+    document.getElementById('transaction-container').appendChild(div);
+
+})
